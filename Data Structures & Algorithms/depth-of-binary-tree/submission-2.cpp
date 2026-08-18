31c41e28e0394257761512c363a0d1c8dfa6fc6c
Nguyen Anh Thien <tn386786@gmail.com>
2026-08-18 15:08:09 +0700

    Add: depth-of-binary-tree - submission-2

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-2.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        // #way 1 use queue
+        // if (root == nullptr){
+        //     return 0;
+        // }
+        // queue<TreeNode *>q;
+        // q.push(root);
+        // int level = 0;
+        // while(!q.empty()){
+        //     size_t levelsSize = q.size();
+        //     for (size_t i = 0; i < levelsSize; i++){
+        //         TreeNode *current = q.front();
+        //         q.pop();
+        //         if (current->left != nullptr){
+        //             q.push(current->left);
+        //         }
+        //         if (current->right != nullptr){
+        //             q.push(current->right);
+        //         }
+        //     }
+        //     level++;
+        // }
+        // return level;
+        // #way 2: use recursion
+        // if (root == nullptr){
+        //     return 0;
+        // }
+        // return 1 + max(maxDepth(root->left), maxDepth(root->right));
+        // #way 3: use stack
+        stack<pair<TreeNode*, int>> stack;
+        stack.push({root, 1});
+        int res = 0;
+
+        while (!stack.empty()) {
+            pair<TreeNode*, int> current = stack.top();
+            stack.pop();
+            TreeNode* node = current.first;
+            int depth = current.second;
+
+            if (node != nullptr) {
+                res = max(res, depth);
+                stack.push({node->left, depth + 1});
+                stack.push({node->right, depth + 1});
+            }
+        }
+        return res;
+    }
+};
