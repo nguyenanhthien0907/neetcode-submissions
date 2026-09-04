9370a0ed73ad592cc2932386f11b8d51d7bb0aaf
Nguyen Anh Thien <tn386786@gmail.com>
2026-09-04 18:00:05 +0700

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        // for (int i = 0; i < nums.size(); i++){
+        //     for (int j = i + 1; j < nums.size(); j++){
+        //         if (nums[i] + nums[j] == target){
+        //             return {i,j};
+        //         }
+        //     }
+        // }
+        // return {};
+        //====================
+        unordered_map<int,int> indices;
+        for (int i = 0; i < nums.size(); i++){
+            indices[nums[i]] = i;
+        }
+
+        for (int i = 0; i < nums.size(); i++){
+            int diff = target - nums[i];
+            if (indices.count(diff) && indices[diff] != i){
+                return {i, indices[diff]};
+            }
+        }
+        return {};
+    }
+};
